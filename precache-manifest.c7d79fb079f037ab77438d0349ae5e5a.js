a55e692698447a61cffec30166e48b1b6ef32af8
Vishal <poison.iitr@gmail.com>
2020-01-30 20:48:28 +0530

    Updates

---
+++ precache-manifest.c7d79fb079f037ab77438d0349ae5e5a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a87a5944fe74c85038e12284304a8ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fe8a2e26485d36617e4",
+    "url": "/static/css/2.17b9c65b.chunk.css"
+  },
+  {
+    "revision": "f3f95e152626cb9d7c75",
+    "url": "/static/css/main.208ef314.chunk.css"
+  },
+  {
+    "revision": "1fe8a2e26485d36617e4",
+    "url": "/static/js/2.d9e33937.chunk.js"
+  },
+  {
+    "revision": "919bf9b21bd9abc17b3df0e60281f96e",
+    "url": "/static/js/2.d9e33937.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3f95e152626cb9d7c75",
+    "url": "/static/js/main.ca978ccc.chunk.js"
+  },
+  {
+    "revision": "2c6fa1f682af69b611bb",
+    "url": "/static/js/runtime-main.ef2cfd25.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
